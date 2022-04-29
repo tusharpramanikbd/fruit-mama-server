ce3783acac6d9e9c7de13dd6ccefdb10387df190
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-29 19:28:44 +0600

    cors and express.json middleware added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+//Api
 app.get('/', (req, res) => {
   res.send('Server working successfully')
 })
