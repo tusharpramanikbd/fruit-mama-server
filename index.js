12ec3cdbaed60616edb467a415ba6785cbd4cead
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-01 19:04:10 +0600

    delete item by id api implemented

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
       )
       res.send(result)
     })
+
+    app.delete('/fruits/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await fruitCollection.deleteOne(query)
+      res.send(result)
+    })
   } finally {
     // await client.close()
   }
